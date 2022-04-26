ea99058cc1f679cd6461fcd8a5086e33bf430332
ERICK_ZABALA <erickzabala@hotmail.com>
2022-04-25 22:48:59 -0400

    Add Dependencies React Native

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//Dependencias ---> React Navigation PANTALLAS
+//import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// Se importa los archivos Login.js, CreateUser.js UserDetail.js, y UserList.js
+import Login from './Screen/Login';
+import Location from './Screen/Location';
+import Result from './Screen/Result';
+
+
+const Stack = createNativeStackNavigator();
+
+function MyStack(){
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Location" component={Location} options={{title:'Location'}} />
+      <Stack.Screen name="Result" component={Result} options={{title:'Result'}} />
+      <Stack.Screen name="Login" component={Login} options={{title:'Login'}} />
+    </Stack.Navigator>
+
+  )
+
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
+
   );
 }
 
